a63d1307757739e3a977e7e8acb3c7fa4e7999bc
leah0913 <46901930+leah0913@users.noreply.github.com>
2019-08-15 11:03:08 -0400

    First Upload

---
+++ assets/data/computerData.ts
@@ -0,0 +1,20 @@
+import { Computer } from '../../app/computer';
+
+export const COMPUTER: Computer[] = [
+    {
+        name: 'com1',
+        cpu: 'i7',
+        make: 'HP'
+    },
+    {
+        name: 'com2',
+        cpu: 'i5',
+        make: 'Samsung'
+    },
+    {
+        name: 'com3',
+        cpu: 'i3',
+        make: 'Apple'
+    }
+
+]
